61cc0976d0091b818e4877558c49356a0798f23f
BADIN <badin71.by@gmail.com>
2016-09-20 16:24:33 +0700

    Up Radio

--- app/src/main/java/th/ac/sut/library/clremfriend/MainActivity.java
+++ app/src/main/java/th/ac/sut/library/clremfriend/MainActivity.java
@@ -1,7 +1,9 @@
 package th.ac.sut.library.clremfriend;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }   // Main Method
+
+    // Get Event From Click Butoon
+
+    public void clickSignUpMain(View view){
+        startActivity(new Intent(MainActivity.this, SignUpActivity.class));
     }
 }   // Main Class นี่คือคลาสหลัก
 
