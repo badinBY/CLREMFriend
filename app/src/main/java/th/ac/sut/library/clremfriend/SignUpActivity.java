1cdea181aba7fb87f2af865537c627c3119f482c
BADIN <badin71.by@gmail.com>
2016-09-21 12:04:31 +0700

    Day21 Sep Befor Lunch

--- app/src/main/java/th/ac/sut/library/clremfriend/SignUpActivity.java
+++ app/src/main/java/th/ac/sut/library/clremfriend/SignUpActivity.java
@@ -2,14 +2,49 @@ package th.ac.sut.library.clremfriend;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    // Explicit การประกาศตัวแปร
+    private EditText nameEditText, addressEditText, phoneEditText,
+            userEditText, passwordEditText;
+    private String nameString, addressString, phoneString,
+            userString, passwordString, genderString, imageString;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        //Bind Widget การผูกตัวแปร
+        nameEditText = (EditText) findViewById(R.id.editText);
+        addressEditText = (EditText) findViewById(R.id.editText2);
+        phoneEditText = (EditText) findViewById(R.id.editText3);
+        userEditText = (EditText) findViewById(R.id.editText4);
+        passwordEditText = (EditText) findViewById(R.id.editText5);
+
+
+
     }   // Main Method
+
+    public void clickSignUpSign(View view) {
+
+        // Get Value From Edit Text
+        nameString = nameEditText.getText().toString().trim();
+        addressString = addressEditText.getText().toString().trim();
+        phoneString = phoneEditText.getText().toString().trim();
+        userString = userEditText.getText().toString().trim();
+        passwordString = passwordEditText.getText().toString().trim();
+
+
+
+
+    } // clickSign
+
 
 
 
