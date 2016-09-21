93eb2bfea01cbbb636260a030f9837084d81054e
BADIN <badin71.by@gmail.com>
2016-09-21 13:15:10 +0700

    Day21 Sep After Lunch My Alert

--- app/src/main/java/th/ac/sut/library/clremfriend/SignUpActivity.java
+++ app/src/main/java/th/ac/sut/library/clremfriend/SignUpActivity.java
@@ -40,6 +40,16 @@ public class SignUpActivity extends AppCompatActivity {
         userString = userEditText.getText().toString().trim();
         passwordString = passwordEditText.getText().toString().trim();
 
+        //Check Space
+        if (nameString.equals("") || addressString.equals("") ||
+                phoneString.equals("") || userString.equals("") || passwordString.equals("")) {
+            //Have Space
+            MyAlert myAlert = new MyAlert(this, R.drawable.doremon48, "มีช่องว่าง", "กรุณากรอกทุกช่อง ค่ะ");
+            myAlert.myDialog();
+
+
+        }
+
 
 
 
