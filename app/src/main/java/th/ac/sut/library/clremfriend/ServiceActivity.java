48bebd1318a3c4cc48bd1454b98713495c5fa5c9
BADIN <badin71.by@gmail.com>
2016-09-23 13:27:07 +0700

    Day23 Welcom User

--- app/src/main/java/th/ac/sut/library/clremfriend/ServiceActivity.java
+++ app/src/main/java/th/ac/sut/library/clremfriend/ServiceActivity.java
@@ -2,12 +2,40 @@ package th.ac.sut.library.clremfriend;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class ServiceActivity extends AppCompatActivity {
+
+    //Explicit
+    private TextView textView;
+    private ListView listView;
+    private String loginString;
+    private String[] nameStrings, imageStrings, genderStrings, addressStrings, phoneStrings;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service);
-    }
-}
+
+        //Bind Widget
+        textView = (TextView) findViewById(R.id.textView8);
+        listView = (ListView) findViewById(R.id.listView);
+
+        //Get Valure from Intent
+        loginString = getIntent().getStringExtra("Login");
+        nameStrings = getIntent().getStringArrayExtra("Name");
+        imageStrings = getIntent().getStringArrayExtra("Image");
+        genderStrings = getIntent().getStringArrayExtra("Gender");
+        addressStrings = getIntent().getStringArrayExtra("Address");
+        phoneStrings = getIntent().getStringArrayExtra("Phone");
+
+        //Show Text
+        textView.setText("Welcome " + loginString);
+
+
+    }   // Main Method
+
+}   // Main Class
